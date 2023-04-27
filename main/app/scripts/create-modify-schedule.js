b0d007cdd434db7fe8c1dd947fc2d7e03f13caa0
Khaled-km1805078 <km1805078@qu.edu.qa>
2023-04-27 19:33:48 +0300

    Create/modify page modals and buttons

--- main/app/scripts/create-modify-schedule.js
+++ main/app/scripts/create-modify-schedule.js
@@ -1,3 +1,4 @@
+//============================accordion setup======================
 var acc = document.getElementsByClassName("accordion");
 var i;
 
@@ -12,3 +13,64 @@ for (i = 0; i < acc.length; i++) {
         }
     });
 }
+//===================================================
+
+
+//===============Modals==============
+// Get the modals
+var modal = document.getElementById("new-date-Modal");
+var modal1 = document.getElementById("new-session-modal");
+var modal2 = document.getElementById("add-event-modal");
+var modal3 = document.getElementById("edit-event-Modal");
+var modal4 = document.getElementById("edit-session-modal");
+
+// Get the buttons that open the modals
+var btn  = document.getElementById("add-date-btn");
+var btn1 = document.getElementById("add-session-btn");
+var btn2 = document.getElementById("add-event-btn");
+var btn3 = document.getElementById("edit-event-btn");
+var btn4 = document.getElementById("edit-session-btn");
+
+// Get the <span> elements that close the modals
+var span = document.getElementsByClassName("close");
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+btn1.onclick = function() {
+  modal1.style.display = "block";
+}
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
+btn3.onclick = function() {
+  modal3.style.display = "block";
+}
+btn4.onclick = function() {
+  modal4.style.display = "block";
+}
+
+// When the user clicks on x, close the modal
+for (var i = 0; i < span.length; i++) {
+  span[i].onclick = function() {
+    var modalToClose = this.parentNode.parentNode;
+    modalToClose.style.display = "none";
+  }
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  } else if (event.target == modal1) {
+    modal1.style.display = "none";
+  } else if (event.target == modal2) {
+    modal2.style.display = "none";
+  } else if (event.target == modal3) {
+    modal3.style.display = "none";
+  } else if (event.target == modal4) {
+    modal4.style.display = "none";
+  }
+}
+//===================================================
+
