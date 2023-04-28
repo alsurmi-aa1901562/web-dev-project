4aa135ed7c23b2dc4888c3bc64b4b5d29cb351b3
Ahmed Mansoor Al-Surmi <aa1901562@qu.edu.qa>
2023-04-28 11:15:15 +0300

    Updated create-modify-schedule.js
    1. Added the creation of session elements.

--- main/app/scripts/create-modify-schedule.js
+++ main/app/scripts/create-modify-schedule.js
@@ -6,24 +6,78 @@ async function createEvents() {
 
 }
 
-// Method that Creates Session Elements
-async function createSession() {
-
-}
-
 // Method that Creates Day Elements
-async function createDay(session, underSchedule) {
-  const daysSection = document.createElement("section");
-  daysSection.id = "day-schedule";
+async function createDay(session, underSchedule, scheduleIndex, sessionIndex) {
+  const daySchedule = document.createElement("section");
+  daySchedule.id = `day-schedule`;
+  daySchedule.setAttribute("scheduleIndex", `${scheduleIndex}`);
+  daySchedule.setAttribute("sessionIndex", `${sessionIndex}`);
 
-  const openDateBtn = document.createElement("button");
-  openDateBtn.classList = "accordion";
-  openDateBtn.innerHTML = `${session.date}`;
-  underSchedule.appendChild(openDateBtn);
-  underSchedule.appendChild(daysSection);
-  // const sessionSection = document.createElement("section");
-  // sessionSection.id = "session-enclosure";
-  // sessionSection.classList = "panel"
+    const openDateBtn = document.createElement("button");
+    openDateBtn.classList = "accordion";
+    openDateBtn.innerHTML = `${session.date}`;
+
+    const sessionSection = document.createElement("section");
+    sessionSection.id = `session-enclosure`;
+    sessionSection.setAttribute("scheduleIndex", `${scheduleIndex}`);
+    sessionSection.setAttribute("sessionIndex", `${sessionIndex}`)
+    sessionSection.classList = "panel";
+
+      const sessionHeading = document.createElement("div");
+      sessionHeading.id = "session-heading";
+      sessionHeading.setAttribute("scheduleIndex", `${scheduleIndex}`);
+      sessionHeading.setAttribute("sessionIndex", `${sessionIndex}`)
+
+        const sessionHeadingParagraph = document.createElement("p");
+        sessionHeadingParagraph.innerHTML = "Sessions";
+
+        const sessionHeadingBtn = document.createElement("button");
+        sessionHeadingBtn.id = "add-session-btn";
+        sessionHeadingBtn.setAttribute("scheduleIndex", `${scheduleIndex}`);
+        sessionHeadingBtn.setAttribute("sessionIndex", `${sessionIndex}`)
+        sessionHeadingBtn.setAttribute("aria-hidden", "true");
+
+          const sessionHeadingBtnIcon = document.createElement("icon");
+          sessionHeadingBtnIcon.classList = "fa fa-plus";
+          sessionHeadingBtn.innerHTML =`${sessionHeadingBtnIcon} Add Session`;
+
+        sessionHeadingBtn.appendChild(sessionHeadingBtnIcon);
+      
+      sessionHeading.appendChild(sessionHeadingParagraph);
+      sessionHeading.appendChild(sessionHeadingBtn);
+
+      const sessionDiv = document.createElement("div");
+      sessionDiv.id = "session";
+      sessionDiv.setAttribute("scheduleIndex", `${scheduleIndex}`);
+      sessionDiv.setAttribute("sessionIndex", `${sessionIndex}`);
+
+        const sessionBtn = document.createElement("button");
+        sessionBtn.classList = "accordian";
+        sessionBtn.id = "session-title";
+        sessionBtn.setAttribute("scheduleIndex", `${scheduleIndex}`);
+        sessionBtn.setAttribute("sessionIndex", `${sessionIndex}`);
+        sessionBtn.innerHTML = `${session.title}`;
+
+        const sessionEventDiv = document.createElement("div");
+        sessionEventDiv.id = "event-closure";
+        sessionEventDiv.classList = "panel";
+        sessionEventDiv.setAttribute("scheduleIndex", `${scheduleIndex}`);
+        sessionEventDiv.setAttribute("sessionIndex", `${sessionIndex}`);
+
+          session.events.forEach((e, i) => {
+            //TODO: create events!
+          });
+
+      sessionDiv.appendChild(sessionBtn);
+
+      sessionSection.appendChild(sessionHeading);
+      sessionSection.appendChild(sessionDiv);
+  
+  daySchedule.appendChild(openDateBtn);
+  daySchedule.appendChild(sessionSection);
+
+  underSchedule.appendChild(daySchedule);
+  
 }
 
 // Method that Load Schedules and Creates Required Elements
@@ -39,7 +93,7 @@ async function loadSchedules(schedules) {
     daysList.id = "all-days";
 
       e.sessions.forEach((e, sessionsIndex) => {
-        createDay(e, daysList);
+        createDay(e, daysList,schedulesIndex, sessionsIndex);
       });
 
     mainDiv.appendChild(scheduleDate);
@@ -61,6 +115,13 @@ async function loadSchedules(schedules) {
 
 // Default DOM
 document.addEventListener("DOMContentLoaded", async () => {
+  // Grabbing Saves From Login
+  const getLogInfo = JSON.parse(localStorage.getItem("logInfo"));
+ 
+  document.getElementById("Nav-userName").innerHTML = `Username: ${getLogInfo.username.replace("@organizer.com", "")}`
+  document.getElementById("Nav-Id").innerHTML = `ID: ${getLogInfo.identity}`;
+
+
   //TODO: Validate if the Schedule exists.. if it does then show the days and if not show a button that asks to create a schedule
   const res = await fetch(schedulesURL);
   const schedules = await res.json();
