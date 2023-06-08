5f0bc4dbd0fc39f2597e2c120b37132a5ff378bc
Ahmed Mansoor Al-Surmi <aa1901562@qu.edu.qa>
2023-06-08 10:54:34 +0300

    Update page.js

--- main/api/app/page.js
+++ main/api/app/page.js
@@ -2,7 +2,6 @@
 export default async function Home() {
   const response = await fetch("http://localhost:3000/api/report");
   const report = await response.json();
-  console.log(report);
   return (
   <body >
     <header>
